3a3a2b6e23dc5e823394e8b661fb2b923f9def20
Luanha2003 <Luanha2003@users.noreply.github.com>
2026-02-13 20:17:58 +0700

    cập nhật giao diện mobile

--- obj/Debug/net9.0/MovieWeb.AssemblyInfo.cs
+++ obj/Debug/net9.0/MovieWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0792b6be070861acbe4e7a10101254f0b8b17d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44abdcaaca45918c0dfde828c5f30755c6959606")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
