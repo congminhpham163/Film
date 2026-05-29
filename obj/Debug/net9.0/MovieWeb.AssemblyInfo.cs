55cfb20c727381e58bc7a81edf13ba19c0c16f56
congminhpham163 <congminhpham163@gmail.com>
2026-05-29 22:27:04 +0700

    fix preview

--- obj/Debug/net9.0/MovieWeb.AssemblyInfo.cs
+++ obj/Debug/net9.0/MovieWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cb3ab5cb0aad965ed66c2e37737139253a970c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be5a08971596d3b96653842650d0757050b0cb58")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
